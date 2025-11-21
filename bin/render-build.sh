c25c3e2007b13414e54ef59e44b8a8905abb84a4
Chris <kchrismucheke@gmail.com>
2025-11-21 14:12:43 +0300

    fix: configure Devise to use `DEVISE_SECRET_KEY` from environment and add `SECRET_KEY_BASE` fallback for asset precompilation.

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,6 +2,11 @@
 # exit on error
 set -o errexit
 
+# Workaround for asset precompilation failure when RAILS_MASTER_KEY is missing or invalid
+if [[ -z "${SECRET_KEY_BASE}" ]]; then
+  export SECRET_KEY_BASE=3f8f9f511365fc3922ea9188564dd354caede29c02caa9fa1329b5d7345d95184972fb0df0c19d1744543de0d9cd6edf5533aa5eb5d184327be28637cb004d7d
+fi
+
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -15,6 +15,7 @@ Devise.setup do |config|
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
   # config.secret_key = '44a41a383f1d247b9888b982d22bc49042ed1520762c7eb0699fa470e881d3f6c3099f19e439215c98998ad7f5376d65cf8b150b764ff29b756f1dc5aeb84872'
+  config.secret_key = ENV['DEVISE_SECRET_KEY'] if ENV['DEVISE_SECRET_KEY'].present?
 
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
